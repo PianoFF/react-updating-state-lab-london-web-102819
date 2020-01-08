52d384dc784453037ae96b13a98a97b4d35c4cdd
PianoFF <Fangfei1star@gmail.com>
2020-01-08 11:48:17 +0000

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+class DigitalClicker extends Component{
+    state = {
+        timesClicked: 0
+    }
+    handleClicker = (e) => {
+        this.setState( previousState => {
+            return {
+                timesClicked: previousState.timesClicked + 1
+            }
+        })
+    }
+    render(){
+        return(
+            <button 
+            id="clicker" 
+            onClick={this.handleClicker}
+            >{this.state.timesClicked}</button>
+        )
+    }
+}
+export default DigitalClicker; 
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,52 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+
+class YouTubeDebugger extends Component{
+    state = {
+        errors: [],
+        user: null,
+        settings: {
+            bitrate: 8,
+            video: {
+            resolution: '1080p'
+            }
+        }
+    }
+
+    handleBitrateClick = (e) => {
+        this.setState ({
+            settings:{
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    handleResolutionClick = (e) => {
+        this.setState ({
+            settings:{
+                ...this.state.settings,
+                video:{
+                    ...this.state.settings.video,
+                    resolution:'720p'
+                }
+            }
+        })
+    }
+    render(){
+        return (
+            <div>
+                <button
+                    className='bitrate'
+                    onClick={this.handleBitrateClick}
+                >bitrate!</button>
+                <button
+                    className='resolution'
+                    onClick={this.handleResolutionClick}
+                >Resolution!</button>
+            </div>
+        )
+    }
+}
+
+export default YouTubeDebugger; 
